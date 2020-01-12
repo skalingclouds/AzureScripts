da0aaaec1e9c93ba603961f678252a412b20a5ab
SkalingClouds <chris.skaling@gmail.com>
2020-01-12 15:03:53 -0800

    final two scripts

--- build-fleetazvm.ps1
+++ build-fleetazvm.ps1
@@ -1,11 +1,34 @@
 Param (
+[Parameter(Mandatory=$true)] 
 [string] $resourceGroup,
+[Parameter(Mandatory=$true)] 
 [string] $basecomputername,
+[Parameter(Mandatory=$true)] 
 [string] $keyvaultname,
+[Parameter(Mandatory=$true)] 
+[string] $region,
+[Parameter(Mandatory=$true)] 
 [int32] $numberofvms
 )
+#$region = "West US 2"
+$count = 0
+$vms = Get-AzVm 
+foreach ($vm in $vms) {
+    $count = $count + 1
+    New-Variable -Scope Global -Force -Name $count -Value $vm
+    $vmname = $vm.name
+    write-host "Select $count for $vmname"
+}
+$govm = read-host "what is your selection"
+if ($govm -eq 1){
+    $selectedvmname = $1.Name
+    $selectedvmrg = $1.ResourceGroupName
+    write-host  "I will connect you to $selectedvmname at resource group $selectedvmrg"
+}
 
-$count = 1
+
+$vmImageid = (Get-AzGalleryImageDefinition -ResourceGroupName "sc-coreinfra-01" -GalleryName scsig01).id
+
 $username = (Get-AzKeyVaultSecret -vaultName $keyvaultname -name "VMUserName").SecretValueText
 $username
 $password = (Get-AzKeyVaultSecret -vaultName $keyvaultname -name "VMPassword").SecretValueText
@@ -13,25 +36,48 @@ $password
 $VMLocalAdminUser = $username
 $VMLocalAdminSecurePassword = ConvertTo-SecureString $password -AsPlainText -Force
 $Credential = New-Object System.Management.Automation.PSCredential ($VMLocalAdminUser, $VMLocalAdminSecurePassword);
-# Start of the loop.
-while ($count -le $numberofvms){
 
-New-AzVm `
-    -ResourceGroupName $resourceGroup `
-    -Name "fleetvm$count" `
-    -Location "West US 2" `
-    -VirtualNetworkName "FleetVnet" `
-    -SubnetName "FleetSubnet" `
-    -SecurityGroupName $basecomputername"sg"$count `
-    -PublicIpAddressName $basecomputername"ip"$count `
-    -OpenPorts 80,3389 `
-    -credential $Credential
+New-AzResourceGroup -Name $resourceGroup -location $region
+
+$subnetConfig = New-AzVirtualNetworkSubnetConfig -Name mySubnet -AddressPrefix 192.168.1.0/24
+
+$vnet = New-AzVirtualNetwork -ResourceGroupName $resourceGroup -location $region -Name MYvNET -AddressPrefix 192.168.0.0/16 -Subnet $subnetConfig
+
+while ($count -le $numberofvms) {
     
-    # Increase the value of $count by 1.
-    # This is equivalent to $count = $count + 1
-    $count++
+    $count = $count + 1
+    $govmname = "$basecomputername$count"
+    $nsgname = "nsg$govmname"
+    $nicname = "nic$govmname"
+    $pipname = "pip$govmname"
+    $nsgrulename = "nsgrule$govmname"
 
+    $pip = New-AzPublicIpAddress -ResourceGroupName $resourceGroup -location $region -Name $pipname -AllocationMethod Static -IdleTimeoutInMinutes 4
+    
+    $nsgRuleRDP = New-AzNetworkSecurityRuleConfig -Name $nsgrulename -Protocol Tcp `
+    -Direction Inbound -Priority 1000 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * `
+    -DestinationPortRange 3389 -Access Allow
+    
+    $nsg = New-AzNetworkSecurityGroup -ResourceGroupName $resourceGroup -location $region `
+    -Name $nsgname -SecurityRules $nsgRuleRDP
+
+    $nic = New-AzNetworkInterface -Name $nicname -ResourceGroupName $resourceGroup -location $region `
+    -SubnetId $vnet.Subnets[0].Id -PublicIpAddressId $pip.Id -NetworkSecurityGroupId $nsg.Id
+    
+    # Create a virtual machine configuration using $imageVersion.Id to specify the shared image
+    $vmConfig = New-AzVMConfig -VMName $govmname -VMSize Standard_D1_v2 | `
+    Set-AzVMOperatingSystem -Windows -ComputerName $govmname -Credential $Credential | `
+    Set-AzVMSourceImage -Id $vmImageid | `
+    Add-AzVMNetworkInterface -Id $nic.Id
+    
+    # Create a virtual machine
+    New-AzVM -ResourceGroupName $resourceGroup -location $region -VM $vmConfig
+
+    
 }
+
 Write-host "You now have $numberofvms built, enjoy your fleet" 
 # End of the loop. 
 # The loop will continue while $count is less than or equal to the $iterations variable set above.
+#Get-AzPublicIpAddress -ResourceGroupName "myResourceGroup" | Select "IpAddress"
+#mstsc /v:publicIpAddress
---
+++ rdp-toazvm.ps1
@@ -0,0 +1,43 @@
+$keyvaultname = "SkalingCloudsVault"
+$vms = (get-azvm).Name 
+$username = (Get-AzKeyVaultSecret -vaultName $keyvaultname -name "VMUserName").SecretValueText
+$username
+$password = (Get-AzKeyVaultSecret -vaultName $keyvaultname -name "VMPassword").SecretValueText
+$password
+Function Show-Menu {
+    Param(
+        [String[]]$vms
+    )
+    do { 
+        Write-Host "Please make a selection"
+        $index = 1
+        foreach ($vm in $vms) {
+            Write-Host "[$index] $vm"
+            $index++
+        }
+        $Selection = Read-Host 
+    } until ($vms[$selection-1])
+    
+    $selectedvm = $($vms[$selection-1])
+    
+    Write-Verbose "You selected $selectedvm" -Verbose
+    
+    $vmrg = (get-azvm -Name $selectedvm).ResourceGroupName
+    $nicname =  (get-azvm -Name $selectedvm).NetworkProfile.NetworkInterfaces.Id.Split('/')[-1]
+    $nicobject = (Get-AzNetworkInterface -name $nicname)
+    $privateipaddress = $nicobject.IpConfigurations.PrivateIpAddress
+    $publiciptest = (Get-azNetworkInterface -ResourceGroupName $vmrg -Name $nicname).IpConfigurations.PublicIpAddress.Id
+    If ($publicIpTest){
+        $publicIpName =  (Get-azNetworkInterface -ResourceGroupName $vmrg -Name $nicname).IpConfigurations.PublicIpAddress.Id.Split('/')  | Select-Object -Last 1
+        $publicIpAddress = (Get-AzureRmPublicIpAddress -ResourceGroupName $vmrg -Name $publicIpName).IpAddress
+        Write-Host "Public IP Exists, Connecting to $vmName via $publicIpAddress"
+        cmdkey.exe /generic:$publicIpAddress /user:$username /pass:$password
+        mstsc.exe /v $publicIpAddress /f
+    } Else {
+        cmdkey.exe /generic:$privateIpAddress /user:$username /pass:$password
+        Write-Host "No Public IP, Connecting over Private IP"
+        mstsc.exe /v $privateipaddress /f 
+    }
+}
+$Selection = Show-Menu -vms $vms
+
